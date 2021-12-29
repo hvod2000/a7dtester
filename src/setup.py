7b367acce5a879f4e1851b695e9f07b7269e45f1
Uladzislau Khamkou <kham@tuta.io>
2021-12-29 20:44:45 +0300

    version 0.0.2 -> 0.0.3

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open("../readme.md", "r") as f:
 
 setuptools.setup(
     name="a7dtester",
-    version="0.0.2",
+    version="0.0.3",
     author="Uladzislau Khamkou",
     description="simple test runner",
     long_description=long_description,
@@ -15,5 +15,5 @@ setuptools.setup(
     py_modules=["a7dtester"],
     entry_points = {'console_scripts': ['test_with_a7dtester=a7dtester:main']},
     package_dir={"": "."},
-    install_requires=[],
+    install_requires=['a7d>=0.0.5'],
 )
